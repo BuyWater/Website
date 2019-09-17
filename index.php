9fc1ab7d7dc34a93e6ce47b09528d78cd9229f01
YeetYeet Jack <jackfratto@gmail.com>
2019-09-17 12:48:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
-<!DOCTYPE html>
 <html>
 <body>
 
-<div id="demo">
-<h2>The XMLHttpRequest Object</h2>
-<button type="button" onclick="loadDoc()">Change Content</button>
-</div>
-  
-<?php
-  $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-  $txt = "jack Fratto was here\n";
-  fwrite($myfile, $txt);
-  $txt = "New line here entered\n";
-  fwrite($myfile, $txt);
-  fclose($myfile);
-?>
+<form action="welcome.php" method="Get">
+  Name <input type="text" name="name"><br>
+  Email<input type="text" name="email"><br>
+  <input type="submit">
+</form>
 
 </body>
 </html>
