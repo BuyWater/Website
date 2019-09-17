bf2f026bded371f32fdd406da2dc2faafa4b2f50
YeetYeet Jack <jackfratto@gmail.com>
2019-09-17 12:14:28 -0400

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<div id="demo">
+<h2>The XMLHttpRequest Object</h2>
+<button type="button" onclick="loadDoc()">Change Content</button>
+</div>
+
+<script>
+<?php
+  $myFile = fopen("myText.txt", "w") or die("Couldn't open file!")
+  $txt = "jack Fratto was here\n";
+  fwrite($myFile, $txt);
+  $txt = "New line here entered\n";
+  fwrite($myFile, $txt);
+  fclose($myFile);
+?>
+</script>
+
+</body>
+</html>
