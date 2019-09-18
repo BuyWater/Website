f6ec52034b31ccc9b54ccd976f4e3504591111a1
YeetYeet Jack <jackfratto@gmail.com>
2019-09-17 20:25:42 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <!DOCTYPE HTML>
 <html>
+<head>
+<style>
+#rcorners1 {
+  border-radius : 25px;
+  background : #73AD21;
+  padding : 20px;
+  width : 50px;
+  height : 50px;
+}
+</style>
+</head>
 <body>
-
-<?php
-  $myfile = fopen("newfile.txt", "w") or die ("Couldn't open file for some stupid reason");
-  $txt = "Jack wuz here";
-  fwrite($myfile, $txt);
-  fclose("myfile.txt");
-?>
- 
+  <div id="commentBox">
+    Name : <input type="text" name="name">
+    Comment : <input type="text" name="text">
+  </div>
+  <script>
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+      if (this.readyState == 4 && this.status == 200){
+        document.getElementById("commentBox").innerHTML = this.responseText;
+      }
+    }
+    xhr.open("GET", "postComment.php", true);
+    xhr.send();
+    
+  </script>
 </body>
 </html>
